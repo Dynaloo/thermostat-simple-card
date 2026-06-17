2d5c76b9e6fbe918adce7c76dd15c4b6da86e1cc
Dynaloo <92954018+Dynaloo@users.noreply.github.com>
2026-06-17 09:05:50 +0200

    Add Terser plugin to rollup config
    Added Terser plugin for potential minification.

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,16 @@
 import nodeResolve from '@rollup/plugin-node-resolve';
+import terser from '@rollup/plugin-terser';
 
 export default {
   input: 'src/thermostat-simple-card.js',
   output: {
     file: 'dist/thermostat-simple-card.js',
     format: 'es',
+    sourcemap: false
   },
-  plugins: [nodeResolve()],
+  plugins: [
+    nodeResolve(),
+    // Ligne ci-dessous à décommenter pour minifier le fichier final pour HACS
+    // terser() 
+  ]
 };
